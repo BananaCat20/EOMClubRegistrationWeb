9e97d02f6478599dbe9b3e2bc76631caecb63868
Jessie <bannanacat20@gmail.com>
2022-11-10 10:30:06 -0500

    testrun#2

--- src/main/java/cs/project/eom/ClubRegistrationWeb/AppController.java
+++ src/main/java/cs/project/eom/ClubRegistrationWeb/AppController.java
@@ -16,7 +16,7 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.Model; 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
